a053b504e50c83f9b0557ba74ffede5fab5a54ab
Bayatiko <Bayatyantiko@gmail.com>
2021-06-13 20:34:35 +0400

    new changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function rectangleArea(width, height) {
+    if (width < 0 || height < 0) {
+        return 'You need positive integers to calculate area!';
+    }
+    return width * height;
+}
+
+function rectangleArea(width, height) {
+    if (width < 0 || height < 0) {
+        return 'You need positive integers to calculate area!';
+    }
+    return width * height;
+}
